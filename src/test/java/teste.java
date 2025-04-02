934c48740025c0307f9507848b4066fffa82c7fa
QAzando <qazando@gmail.com>
2025-04-01 22:49:04 -0300

    ADICCIONANDO  NOVOS TESSTES

--- src/test/java/teste.java
+++ src/test/java/teste.java
@@ -1,10 +1,18 @@
 // Importa as classes necessárias para os testes
 import io.restassured.RestAssured; // Biblioteca para automação de testes REST
 import org.junit.jupiter.api.Test; // Framework de testes JUnit 5
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 // Classe que contém os testes de API
 public class teste {
+
+    public String lerJson(String caminhoArquivo) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(caminhoArquivo)));
+    }
 
     // Define um método de teste
     @Test
@@ -20,5 +28,46 @@ public class teste {
         .then() // Define as validações da resposta
             .statusCode(200) // Verifica se o status code da resposta é 200 (OK)
             .log().all();    // Loga no console todos os detalhes da resposta (body, headers, etc.)
+    }
+
+    @Test
+    public void testWithId() {
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+
+        given()
+                .header("Accept", "application/json")
+                .when()
+                .get("/booking/595")
+                .then()
+                .statusCode(200)
+                .body("firstname", equalTo("Josh"))
+                .body("lastname", equalTo("Allen"))
+                .body("totalprice", equalTo(111))
+                .body("depositpaid", is(true))
+                .body("bookingdates.checkin", equalTo("2018-01-01"))
+                .body("bookingdates.checkout", equalTo("2019-01-01"))
+                .body("additionalneeds", equalTo("super bowls"));
+    }
+
+    @Test
+    public void cadastrarReserva() throws IOException {
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+
+        String jsonBody = lerJson("src/test/resources/payloads/reserva.json");
+
+        given()
+                .header("Content-Type", "application/json")
+                .body(jsonBody)
+                .when()
+                .post("/booking")
+                .then()
+                .statusCode(200)
+                .body("booking.firstname", equalTo("herbertao"))
+                .body("booking.lastname", equalTo("qazando"))
+                .body("booking.totalprice", equalTo(222))
+                .body("booking.depositpaid", is(true))
+                .body("booking.bookingdates.checkin", equalTo("2018-01-01"))
+                .body("booking.bookingdates.checkout", equalTo("2019-01-01"))
+                .body("booking.additionalneeds", equalTo("Breakfast"));
     }
 }
